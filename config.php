28aa6d816bc5c4ac09b1a37207742f182b72e4bb
asanchezo <asanchezo@STI-ASANCHEZ.grupoelangel.co.cr>
2016-01-19 09:33:22 -0600

    conexion a db sqlite 2

--- config.php
+++ config.php
@@ -1,31 +1,28 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
+<?php
 
-        class MyDB extends SQLite3 {
+class MyDB extends SQLite3 {
 
-            function __construct() {
-                $this->open('test.sqlite');
-            }
+    function __construct() {
+        $this->open('test.sqlite');
+    }
 
-        }
+}
 
-        $db = new MyDB();
-        if (!$db) {
-            echo $db->lastErrorMsg();
-        } else {
-            echo "Opened database successfully\n";
-        }
-        ?>
-    </body>
-</html>
+$db = new MyDB();
+if (!$db) {
+    echo $db->lastErrorMsg();
+} else {
+    echo "Opened database successfully\n";
+}
+
+$sql = <<<EOF
+      SELECT * FROM prueba;
+EOF;
+
+$ret = $db->query($sql);
+while ($row = $ret->fetchArray(SQLITE3_ASSOC)) {
+    echo "ID = " . $row['id'] . "\n";
+    echo "nombre= " . $row['nombre'] . "\n";
+}
+echo "Operation done successfully\n";
+$db->close();
